b4ad392d955eeb41c49e55f835b57225d2f69ac4
Raiden <549485630@qq.com>
2019-07-16 22:50:32 +0800

    遇到字符串List的时候出现强转报错

--- src/main/java/com/raiden/task/AbstractTask.java
+++ src/main/java/com/raiden/task/AbstractTask.java
@@ -29,32 +29,38 @@ public abstract class AbstractTask implements Task{
         return treeMap;
     }
 
-    /**
-     * 根据字段名排序
-     * @param map
-     * @return
-     */
+
     private Map<String, Object> sortField(Map<String, Object> map){
+        //new 一个排序的Map 在构造方法中传入比较器
         Map<String, Object> treeMap = new TreeMap<>(new MapComparator());
         for (Map.Entry<String, Object> entry : map.entrySet()){
             Object value = entry.getValue();
-            //除去空和空字符
+            //去掉空和空字符串
             if (value == null || (value instanceof String && StringUtils.isBlank((String) value))){
                 continue;
             }
+            //判断是不是 JSONObject 如果是转化成TreeMap
             if (value instanceof JSONObject){
-                JSONObject jsonObject  = (JSONObject) value;
-                treeMap.put(entry.getKey(), sortField(JSON.parseObject(jsonObject.toJSONString(), Map.class)));
+                //JSONObject 覆写了toString 所有可以直接调用toString
+                treeMap.put(entry.getKey(), sortField(JSON.parseObject(value.toString(), Map.class)));
+                //判断是不是 JSONArray
             }else if (value instanceof JSONArray){
+                //如果是 强转成 JSONArray 并且遍历它
                 JSONArray jsonArray = (JSONArray) value;
                 List<Object> list = new ArrayList<>(jsonArray.size());
                 for (Object object : jsonArray){
-                    JSONObject jsonObject  = (JSONObject) object;
-                    Map<String, Object> map1 = sortField(JSON.parseObject(jsonObject.toJSONString(), Map.class));
-                    list.add(map1);
+                    //判断里面的是否是 JSONObject 如果是转化成TreeMap
+                    if (object instanceof JSONObject){
+                        object = sortField(JSON.parseObject(object.toString(), Map.class));
+                    }
+                    //其他基础类型直接放进去
+                    list.add(object);
                 }
+                //list 排序方便比较
+                list.sort(new ListComparator());
                 treeMap.put(entry.getKey(), list);
             }else {
+                //其他基础类型直接放进去
                 treeMap.put(entry.getKey(), value);
             }
         }
@@ -66,6 +72,26 @@ public abstract class AbstractTask implements Task{
         @Override
         public int compare(String str1, String str2) {
             return str1.compareTo(str2);
+        }
+    }
+
+    class ListComparator implements Comparator<Object> {
+        @Override
+        public int compare(Object o1, Object o2) {
+            //因为这里面不是基础类型就是Map 或者他们的 map的toString方法被复写过 会把所有的key和value组合拼接成字符串
+            //比如 map.put("key":"value"); toString 会变成 {"key":"value"}
+            String str1 = o1.toString();
+            String str2 = o2.toString();
+            //获得其字符串长度
+            Integer length1= str1.length();
+            Integer length2 = str2.length();
+            //先比较其长度
+            int i = length1.compareTo(length2);
+            //如果长度相同 在比较实际字符串
+            if (i == 0){
+                return str1.compareTo(str2);
+            }
+            return i;
         }
     }
 }
