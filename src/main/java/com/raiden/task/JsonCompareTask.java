4f8517cfdd53b9dad75607cfa8c4e24128d54782
Raiden <549485630@qq.com>
2019-07-20 10:36:23 +0800

    优化了部分代码

--- src/main/java/com/raiden/task/JsonCompareTask.java
+++ src/main/java/com/raiden/task/JsonCompareTask.java
@@ -59,66 +59,81 @@ public class JsonCompareTask extends AbstractTask{
                     //将左边多余的全部输出
                     leftDocument.insertString(leftDocument.getLength(), LINE_BREAK + leftJsons[leftIndex++], left.getStyle("red"));
                 }else if (rightIndex < rightJsons.length && leftIndex < leftJsons.length){
-                    String leftValue = leftJsons[leftIndex];
-                    StringBuilder leftBuilder = new StringBuilder();
-                    //循环右边的 元素与左边已经取出的元素进行对比 直到对比到相等的或者这个json块的终止符（最外层的右半边大括号）
-                    for (int i = rightIndex; i < rightJsons.length; i++){
-                        String rightValue = rightJsons[i];
-                        //如果遇到是个左半边大括号 就放入栈中
-                        if ("{".equals(rightValue) && i != rightIndex){
-                            stack.push(rightValue);
-                        }
-                        //遇到终结符 且栈不为空就弹栈
-                        if ("}".equals(rightValue) && !stack.empty()){
-                            stack.pop();
-                            //这里也还在其他代码块中 所以也不用比较
-                            continue;
-                        }
-                        //如果栈不为空 则说明进入到其他json块中 不用比较
-                        if (!stack.empty()){
-                            continue;
-                        }
-                        //如果能到这 说明回到了原来代码块中 可以继续比较
-                        //如果遇到了相同的 以原色输出到界面 并且将左边leftBuilder 累加的换回符也一并输出 全部下标+1
-                        if (compare(leftValue, rightValue)) {
-                            //将左边累加的换行一并输出
-                            leftBuilder.append(LINE_BREAK + leftValue);
-                            leftDocument.insertString(leftDocument.getLength(), leftBuilder.toString(), left.getStyle("normal"));
-                            leftIndex++;
-                            //比较是不是第一次比就遇到了正确的 如果是就输出 右边下标+1
-                            if (rightIndex == i){
-                                rightDocument.insertString(rightDocument.getLength(), LINE_BREAK + rightValue, left.getStyle("normal"));
-                                rightIndex++;
-                            }else {
-                                //如果不是 就将右边 rightIndex 位置的字符 到 i - 1 位置的字符全部以红色输出 并且让 rightIndex = i+1
-                                for (int j = rightIndex; j < i;j++){
-                                    rightDocument.insertString(rightDocument.getLength(), LINE_BREAK + rightJsons[j], left.getStyle("red"));
-                                }
-                                rightDocument.insertString(rightDocument.getLength(), LINE_BREAK + rightValue, left.getStyle("normal"));
-                                rightIndex = i + 1;
-                            }
-                            break;
-                        }else {
-                            //如果不相同 就一直比 直到遇到相同的 或者遇到 右半边大括号
-                            //不相同 左边新增一个换行符
-                            leftBuilder.append(LINE_BREAK);
-                            //判断是不是终结符
-                            if ("}".equals(rightValue)){
-                                // 就终止对比 将左边以红色输出 给右边输出加一个空行符
-                                leftDocument.insertString(leftDocument.getLength(), LINE_BREAK + leftJsons[leftIndex++], left.getStyle("red"));
-                                rightDocument.insertString(rightDocument.getLength(), LINE_BREAK, left.getStyle("red"));
-                                //清空左边累加的换行符
-                                leftBuilder.delete(0, leftBuilder.length());
-                                break;
-                            }
-                        }
-                    }
+                    comparativeText(stack, leftDocument, rightDocument, leftJsons, rightJsons, leftIndex, rightIndex);
                 }
                 sum++;
             }
         }catch (Exception e){
             logger.error("JsonCompareTask is error", e);
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * 核心文本比较方法
+     * @param stack
+     * @param leftDocument
+     * @param rightDocument
+     * @param leftJsons
+     * @param rightJsons
+     * @param leftIndex
+     * @param rightIndex
+     * @throws BadLocationException
+     */
+    private void comparativeText(Stack<String> stack,Document leftDocument,Document rightDocument,String[] leftJsons,String[] rightJsons,int leftIndex,int rightIndex) throws BadLocationException {
+        String leftValue = leftJsons[leftIndex];
+        StringBuilder leftBuilder = new StringBuilder();
+        //循环右边的 元素与左边已经取出的元素进行对比 直到对比到相等的或者这个json块的终止符（最外层的右半边大括号）
+        for (int i = rightIndex; i < rightJsons.length; i++){
+            String rightValue = rightJsons[i];
+            //如果遇到是个左半边大括号 就放入栈中
+            if ("{".equals(rightValue) && i != rightIndex){
+                stack.push(rightValue);
+            }
+            //遇到终结符 且栈不为空就弹栈
+            if ("}".equals(rightValue) && !stack.empty()){
+                stack.pop();
+                //这里也还在其他代码块中 所以也不用比较
+                continue;
+            }
+            //如果栈不为空 则说明进入到其他json块中 不用比较
+            if (!stack.empty()){
+                continue;
+            }
+            //如果能到这 说明回到了原来代码块中 可以继续比较
+            //如果遇到了相同的 以原色输出到界面 并且将左边leftBuilder 累加的换回符也一并输出 全部下标+1
+            if (compare(leftValue, rightValue)) {
+                //将左边累加的换行一并输出
+                leftBuilder.append(LINE_BREAK + leftValue);
+                leftDocument.insertString(leftDocument.getLength(), leftBuilder.toString(), left.getStyle("normal"));
+                leftIndex++;
+                //比较是不是第一次比就遇到了正确的 如果是就输出 右边下标+1
+                if (rightIndex == i){
+                    rightDocument.insertString(rightDocument.getLength(), LINE_BREAK + rightValue, left.getStyle("normal"));
+                    rightIndex++;
+                }else {
+                    //如果不是 就将右边 rightIndex 位置的字符 到 i - 1 位置的字符全部以红色输出 并且让 rightIndex = i+1
+                    for (int j = rightIndex; j < i;j++){
+                        rightDocument.insertString(rightDocument.getLength(), LINE_BREAK + rightJsons[j], left.getStyle("red"));
+                    }
+                    rightDocument.insertString(rightDocument.getLength(), LINE_BREAK + rightValue, left.getStyle("normal"));
+                    rightIndex = i + 1;
+                }
+                break;
+            }else {
+                //如果不相同 就一直比 直到遇到相同的 或者遇到 右半边大括号
+                //不相同 左边新增一个换行符
+                leftBuilder.append(LINE_BREAK);
+                //判断是不是终结符
+                if ("}".equals(rightValue)){
+                    // 就终止对比 将左边以红色输出 给右边输出加一个空行符
+                    leftDocument.insertString(leftDocument.getLength(), LINE_BREAK + leftJsons[leftIndex++], left.getStyle("red"));
+                    rightDocument.insertString(rightDocument.getLength(), LINE_BREAK, left.getStyle("red"));
+                    //清空左边累加的换行符
+                    leftBuilder.delete(0, leftBuilder.length());
+                    break;
+                }
+            }
         }
     }
 
