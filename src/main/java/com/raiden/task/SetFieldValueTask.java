60463214ac203b7a44dafe87d8e7b3e6ab531c8f
XINLEI002 <xinlei002@huazhu.com>
2019-08-06 15:39:08 +0800

    修复赋值的bug

--- src/main/java/com/raiden/task/SetFieldValueTask.java
+++ src/main/java/com/raiden/task/SetFieldValueTask.java
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 public class SetFieldValueTask extends AbstractTask{
 
     private static final String LINE_BREAK = "\n";
-    private static final Pattern pattern = Pattern.compile("var (\\w+) = new (\\w+)\\(\\)");
+    private static final Pattern pattern = Pattern.compile("[var|(\\w+)] (\\w+) = new (\\w+)\\(\\)");
     private static final String SET = ".set";
 
     public SetFieldValueTask(JTextPane... jTextPanes){
